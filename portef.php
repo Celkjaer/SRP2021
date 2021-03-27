2a4c2e4fe008f10a5f4f1e369351f3c4488eeff1
Celkjaer <81251967+Celkjaer@users.noreply.github.com>
2021-03-27 12:44:11 +0100

    Create portef.php

---
+++ portef.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Aktielearn</title>
+<link rel="stylesheet" href="portef1.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+
+  <div class="header">
+    <h1>Aktielearn</h1>
+  </div>
+
+  <div class="topnav">
+  <a href="startside.php">Startside</a>
+  <a href="portef.php">Min portefølje</a>
+  <a href="Overblik.php">Aktie overblik</a>
+  </div>
+
+</body>
+</html>
