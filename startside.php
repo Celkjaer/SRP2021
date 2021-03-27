ad24d731ba8c692b2dbab108348dc59c80fd93ea
Celkjaer <81251967+Celkjaer@users.noreply.github.com>
2021-03-27 12:41:10 +0100

    Create startside.php

---
+++ startside.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Aktielearn</title>
+<link rel="stylesheet" href="startside2.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+
+  <div class="header">
+    <h1>Aktielearn</h1>
+  </div>
+
+  <div class="topnav">
+    <a href="startside.php">Startside</a>
+    <a href="portef.php">Min portefølje</a>
+    <a href="Overblik.php">Aktie overblik</a>
+  </div>
+
+  <div class="row">
+  <div class="column">
+    <h1>Hej, velkommen til Aktielearn</h1>
+    <h3> Aktielearn er et program, som hjælper dig frem til, hvordan du bedst muligt investerer i den mest optimale portefølje. <br> Når du skal investere i aktier, er det vigtigt at have noget viden om aktier, og hvordan de kan forløbe sig. <br>
+    Læs med herunder, så du kan lære lidt om aktier, og dermed hvordan du kan blive den bedst mulige investor. </h3> 
+  </div>
+  
+<p>
+  <h4> Risiko og afkast </h4>
+    Når du skal investere i aktier, er det vigtigt, at ud ved hvad risiko og afkast er, fordi i bund og grund handler investering om at opnå det højeste afkast med den laveste risiko. 
+    <br> Et afkast er det økonomiske resultat af din investering. Der kan være tale om et positivt afkast, altså hvor din investering er blevet mere værd, men også om et negativt afkast, altså hvor din investering er blevet mindre værd. <br> Risikoen for din investering angiver, hvor stor sikkerheden er for det fremtidige afkast kan forudsiges eller hvor meget afkastet kan svinge fra et år til et andet. <br> Sammenhængen mellem risiko og afkast kan generelt siges at jo større risiko en investor påtager sig, jo større er chancen for et afkast, som er væsentligt over gennemsnittet. Jo større risiko en investor pådrager sig, jo større er risikoen for at lide et negativt afkast 
+</p>
+      
+<p>
+  <h4> Porteføljer </h4>
+  	Når du skal investere i aktier, er det vigtigt at have forståelse for porteføljer. En portefølje er en samling af dine aktier. Hver gang du køber en aktie, indgår den altså som en del af din portefølje. <br>
+    For at begrænse risikoen ved dine aktier, kan du sprede dine investeringer ud på forskellige aktier. Når du spreder din investering på forskellige aktier, vil du diversificere din portefølje, som overordnet ser betyder at hvis den ene af dine aktier giver et negativt afkast, vil det ikke nødvendigvis komme til at påvirke de andre aktier i din portefølje. 
+  Når du laver din portefølje af aktier, vil hver aktie have en givet vægt i porteføljen.
+</p>
+  
+<p>
+  <h4> Minimum Varians Porteføljen </h4>
+  Minimum Varians Porteføljen (MVP) er den porteføljekombination, hvor risikoen er lavest mulig. Ved at beregne minimum varians porteføljen, finder du altså vægtene af hver aktie til den laveste risiko. 
+Som sagt vil vi gerne finde den mest optimale portefølje. Ud fra vægten på hver aktie i din portefølje, kan du skrue på den, og derved se hvilken vægt, som ud fra minimum varians porteføljen, giver den mest optimale portefølje, altså den laveste risiko med dertilhørende afkast. 
+  
+  
+</p>
+
+
+<br> 
+<a href="Overblik.php" class="button">Forstået, videre</a>
+
+</body>
+</html>
